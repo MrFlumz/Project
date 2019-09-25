c4bb6d2b28e5f07d5c98dc60fd5bdbca4e7aead0
thomas <flumz26@gmail.com>
2019-09-25 12:41:25 +0200

    RFM69 working on HR20

---
+++ WirelessCom/OpenHR20 Custom/openHR20/Debug/makedep.mk
@@ -0,0 +1,26 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+controller.c
+
+eeprom.c
+
+engine.c
+
+get_millis.c
+
+lcd.c
+
+main.c
+
+motor.c
+
+RFM69.c
+
+rtc.c
+
+spi.c
+
