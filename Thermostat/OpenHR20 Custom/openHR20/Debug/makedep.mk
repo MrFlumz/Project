1ec36e590ab70e3f99f8e09e6daa537222832b8d
au568033 <aksel.s.madsen@gmail.com>
2019-09-27 13:50:57 +0200

    Opdateret OpenHR20_custom

---
+++ Thermostat/OpenHR20 Custom/openHR20/Debug/makedep.mk
@@ -0,0 +1,20 @@
+################################################################################
+# Automatically-generated file. Do not edit or delete the file
+################################################################################
+
+adc.c
+
+controller.c
+
+eeprom.c
+
+engine.c
+
+lcd.c
+
+main.c
+
+motor.c
+
+rtc.c
+
