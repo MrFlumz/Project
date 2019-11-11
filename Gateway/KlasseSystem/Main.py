4523bbb5da9c7c353b60fdcaa6b04ac274a812b6
MrFlumz <flumz26@gmail.com>
2019-11-11 11:41:43 +0100

    idk

--- Gateway/KlasseSystem/Main.py
+++ Gateway/KlasseSystem/Main.py
@@ -7,31 +7,63 @@ from RoomModules import Room
 import datetime
 import threading
 
-#Use the Person class to create an object, and then execute the printname method:
+import tornado.ioloop
+import tornado.web
+import tornado.websocket
 
-roomObj = Room(4,33,1)
-roomObj.setDescription("Group locale 123")
+class MainHandler(tornado.web.RequestHandler):
+	def get(self):
+	    self.render("index.html")
 
-#roomObj.addDRTV(3,33)
-#roomObj.addDRTV(10,33)
-#a = datetime.datetime.now()
-#roomObj.getTempAll()
-#roomObj.getValveAll()
-#roomObj.getTargetAll()
-#roomObj.getTargetAll()
-#roomObj.getTempRoom()
-#b = datetime.datetime.now()
-#print(b-a)
+class SimpleWebSocket(tornado.websocket.WebSocketHandler):
+	connections = set()
 
-roomObj.addBUTTON(20,33)
-#roomObj.addTempSensor(11,33)
-roomObj.getTempRoom()
+	def open(self):
+	    self.connections.add(self)
+
+	def on_message(self, message):
+	    [client.write_message(message) for client in self.connections]
+
+	def on_close(self):
+	    self.connections.remove(self)
+
+def make_app():
+	return tornado.web.Application([
+	    (r"/", MainHandler),
+	    (r"/websocket", SimpleWebSocket)
+	])
+
+if __name__ == "__main__":
+	#Use the Person class to create an object, and then execute the printname method:
+
+	roomObj = Room(4,33,1)
+	roomObj.setDescription("Group locale 123")
+	aja = 1
+	#roomObj.addDRTV(3,33)
+	#roomObj.addDRTV(10,33)
+	#a = datetime.datetime.now()
+	#roomObj.getTempAll()
+	#roomObj.getValveAll()
+	#roomObj.getTargetAll()
+	#roomObj.getTargetAll()
+	#roomObj.getTempRoom()
+	#b = datetime.datetime.now()
+	#print(b-a)
+
+	roomObj.addBUTTON(20,33)
+	#roomObj.addTempSensor(11,33)
+	roomObj.getTempRoom()
+	app = make_app()
+	app.listen(8888)
+	
+
+	serverT=threading.Thread(target=tornado.ioloop.IOLoop.instance()).start()
+	x = threading.Thread(target=roomObj.pollButtons()).start()
+	#y = threading.Thread(target=roomObj.pollButtons())
+	#y.start()
+	
 
 
-x = threading.Thread(target=roomObj.pollButtons())
-#y = threading.Thread(target=roomObj.pollButtons())
-#y.start()
-x.start()
 
 
 
